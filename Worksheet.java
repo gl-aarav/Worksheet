96d9084bc2072dce0bcd7dbe88a1fc6ba4764c29
gl-aarav <gl.aarav@gmail.com>
2025-01-28 17:48:41 -0800

    Update Worksheet.java

--- Worksheet.java
+++ Worksheet.java
@@ -19,72 +19,123 @@
  * 	 - prints the problems, name, date, answer key to the Worksheet 
  * 	 - uses PrintWriter
  */ 
- 
- import java.util.Scanner;
- import java.io.*;
 
- public class Worksheet
- {
-	
-	 private int[] firstNumber;
-	 private int[] secondNumber;
-	 private int[] answer;
-	
-	 public Worksheet()
-	 {
-		 firstNumber = new int[20];
-		 secondNumber = new int[20];
-		 answer = new int[20];
-	 }
-	 
-	 public static void main(String[] args)
-	 {
-		 Worksheet ws = new Worksheet();
-		 ws.runIt();
-	 }
-	 
-	 public void runIt()
-	 {
-		 makeIt();
-		 askForNumbers();
-		 generateNumbers();
-		 printWorksheet();
-	 }
-	 
-	 public void makeIt()
-	 {
-		 String outFileName = new String("Worksheet.txt");
-		 PrintWriter output = null;
-		 File outFile = new File(outFileName);
-			try
-			{
-				output = new PrintWriter(outFile);
-			}
-			catch (IOException e)
-			{
-				System.err.println("\n\n\nERROR: Cannot create " + outFileName + " file.\n\n\n");
-				System.exit(2);
-			}
-	 }
-	 
-	 public void askForNumbers()
-	 {
-		 Scanner in = new Scanner(System.in);
-		 int startNum = 0;
-		 int endNum = 0;
-		 System.out.print("Enter the Start Number: ");
-		 startNum = in.nextInt();
-		 System.out.print("Enter the last Number: ");
-		 endNum= in.nextInt();
-	 }
-	 
-	 public void generateNumbers()
-	 {
-		 
-	 }
-	 
-	 public void 	printWorksheet()
-	 {
-		 
-	 }
- }
+import java.util.Scanner;
+import java.io.*;
+
+public class Worksheet
+{
+
+	private int[] firstNumber;
+	private int[] secondNumber;
+	private int[] answer;
+
+	public Worksheet()
+	{
+		final int problemNumber = 20;
+		firstNumber = new int[problemNumber];
+		secondNumber = new int[problemNumber];
+		answer = new int[problemNumber];
+	}
+
+	public static void main(String[] args)
+	{
+		Worksheet ws = new Worksheet();
+		ws.runIt();
+	}
+
+	public void runIt()
+	{
+		PrintWriter output = makeIt();
+		int operation = askForNumbers();
+		printWorksheet(output, operation);
+	}
+
+	public PrintWriter makeIt()
+	{
+		String outFileName = new String("Worksheet.txt");
+		PrintWriter output = null;
+		File outFile = new File(outFileName);
+		try
+		{
+			output = new PrintWriter(outFile);
+		}
+		catch (IOException e)
+		{
+			System.err.println("\n\n\nERROR: Cannot create " + outFileName + " file.\n\n\n");
+			System.exit(2);
+		}
+		return output;
+	}
+
+	public int askForNumbers()
+	{
+	    Scanner in = new Scanner(System.in);
+	    int startNum = 0;
+	    int endNum = 0;
+	    boolean validInput = false;
+	    
+	    while (!validInput) 
+	    {
+	        System.out.print("Enter the Start Value: ");
+	        startNum = in.nextInt();
+	        System.out.print("Enter the End Value: ");
+	        endNum = in.nextInt();
+	        if (startNum < endNum) 
+	        {
+	            validInput = true;
+	        } 
+	        else 
+	        {
+	            System.out.println("End value must be greater than start value. Please try again.");
+	        }
+	    }
+	    int operation = generateNumbers(startNum, endNum);
+	    return operation;
+	}
+
+
+	public int generateNumbers(int start, int end)
+	{
+		int operation = 0;
+		for (int i = 0; i < firstNumber.length-1; i++)
+	    {
+	        operation = (int)(Math.random() * 2) + 1;  // Randomly generate addition or subtraction for each problem
+	        firstNumber[i] = (int)(Math.random() * (end - start)) + start; 
+	        secondNumber[i] = (int)(Math.random() * (end - start)) + start;
+	        if (operation == 1)
+	            answer[i] = firstNumber[i] - secondNumber[i];
+	        else
+	            answer[i] = firstNumber[i] + secondNumber[i];
+	    }
+	    return operation;
+	}
+
+
+	public void printWorksheet(PrintWriter output, int operation)
+	{
+		final int problemsPerLine = 5;
+		String operation2 = new String("");
+		if(operation == 1)
+			operation2 = "-";
+		else
+			operation2 = "+";
+		
+		for (int i = 0; i < firstNumber.length-1; i++) 
+		{
+		    for (int x = 0; x < problemsPerLine; x++) 
+		    {
+		        output.printf("%s", firstNumber[i] + " " + operation2 + " " + secondNumber[i] + " = " + answer[i]);
+		        if (i < firstNumber.length - 1) 
+		        {	
+		            output.print("  ");
+		        }
+		        i++;
+		    }
+		    output.println();
+		}
+
+		output.close();
+	}
+}
+
